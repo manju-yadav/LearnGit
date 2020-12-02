1fb53782f7bde47e908c23b2b6e2d38477b642ea
manju <manju@gmail.com>
2020-12-02 16:18:04 +0530

    commiting in branch1

--- basicCode.java
+++ basicCode.java
@@ -4,6 +4,7 @@ class printString{
 		char abc = 'Print me';
 		int iterate = 2;
 		float num1=1;
+char branchName = 'branch1';
 		for (int i = 0, i <= iterate, i++)
 			System.out.println(abbc);
 	}
