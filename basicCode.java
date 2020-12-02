d59b9e6f8b80cde70676f0f511401f79d053dd36
manju <manju@gmail.com>
2020-12-02 15:40:40 +0530

    added 3 new files

---
+++ basicCode.java
@@ -0,0 +1,10 @@
+class printString{
+	public static void main(String args[])
+	{
+		char abc = 'Print me';
+		int iterate = 2;
+		float num1=1;
+		for (int i = 0, i <= iterate, i++)
+			System.out.println(abbc);
+	}
+}
